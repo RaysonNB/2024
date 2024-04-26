6b3adb7738026ab05e16ad75492b6095f6f1c2d2
RaysonNB <97947041+RaysonNB@users.noreply.github.com>
2024-04-26 16:36:54 +0800

    Update task1_change_cam.py

--- task1_change_cam.py
+++ task1_change_cam.py
@@ -21,6 +21,7 @@ from RobotChassis import RobotChassis
 import datetime
 from std_srvs.srv import Empty
 
+
 class FollowMe(object):
     def __init__(self) -> None:
         self.pre_x, self.pre_z = 0.0, 0.0
@@ -42,7 +43,7 @@ class FollowMe(object):
         b1 = 86.0
         a = a1 * np.pi / 180
         b = b1 * np.pi / 180
-        
+
         d = depth[y][x]
         h, w = depth.shape[:2]
         if d == 0:
@@ -111,9 +112,8 @@ class FollowMe(object):
         image = image.copy()
         depth = depth.copy()
 
-
         frame = image
-        if cx==2000:
+        if cx == 2000:
             cur_x, cur_z = 0, 0
             return cur_x, cur_z, frame, "no"
 
@@ -391,21 +391,25 @@ if __name__ == "__main__":
     print("astra depth")
     _depth1 = None
     _topic_depth1 = "/cam2/depth/image_raw"
-    _sub_up_cam_image = rospy.Subscriber(_topic_image1, Image, callback_image1)
-    #_sub_up_cam_image.unregister()
-    
+    _sub_up_cam_depth = rospy.Subscriber(_topic_image1, Image, callback_image1)
+    # _sub_up_cam_image.unregister()
+
     #
     print("gemini2 rgb")
     _frame2 = None
     _sub_down_cam_image = rospy.Subscriber("/cam1/color/image_raw", Image, callback_image2)
-    time.sleep(2)
-    _sub_down_cam_image.unregister()
+
+
     print("gemini2 depth")
     _depth2 = None
     _sub_down_cam_depth = rospy.Subscriber("/cam1/depth/image_raw", Image, callback_depth2)
-    time.sleep(2)
+
+    time.sleep(5)
+
+    _sub_down_cam_image.unregister()
     _sub_down_cam_depth.unregister()
-    
+    time.sleep(2)
+    print("open up close down")
     s = ""
     print("cmd_vel")
     _cmd_vel = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
@@ -423,9 +427,9 @@ if __name__ == "__main__":
     Kinda = np.loadtxt(RosPack().get_path("mr_dnn") + "/Kinda.csv")
     dnn_yolo1 = Yolov8("bag_japan", device_name="GPU")
     dnn_yolo1.classes = ['obj']
-    
+
     # two yolo
-    
+
     print("pose")
     net_pose = HumanPoseEstimation(device_name="GPU")
 
@@ -454,12 +458,11 @@ if __name__ == "__main__":
     pre_s = ""
     # main var
     t, ee, s = 3.0, "", ""
-    step = "follow"
-
+    step = "get_bag"
 
     clear_costmaps = rospy.ServiceProxy("/move_base/clear_costmaps", Empty)
-    
-    action = "follow"
+
+    action = "none"
     move_turn = "none"
     # wait for prepare
     print("start")
@@ -473,18 +476,23 @@ if __name__ == "__main__":
     # senser var
     class_need = 0
     closest_person = None
-    joint1, joint2, joint3, joint4 = 0,0,0,0
+    joint1, joint2, joint3, joint4 = 0, 0, 0, 0
     set_joints(joint1, joint2, joint3, joint4, 3)
     time.sleep(3)
-    follow_cnt=0
 
-    queue_people_cnt=0
+    queue_people_cnt = 0
     '''
     joint1, joint2, joint3, joint4 = 0.087,1.354,0.758,-1.795
     set_joints(joint1, joint2, joint3, joint4, 1)
     time.sleep(t)'''
-    up_cam_open="yes"
-    down_cam_open="no"
+    #change camera open depth rgb
+
+    sub_hand_cnt=0
+    sub_bag_cnt=0
+    sub_follow_cnt = 0
+
+
+    target_x=-1
     while not rospy.is_shutdown():
         # voice check
         # break
@@ -508,12 +516,10 @@ if __name__ == "__main__":
         down_depth = _depth2.copy()
         up_image = _image1.copy()
         up_depth = _depth1.copy()
-        
-        
-        
-        if step == "get_bag" and up_cam_open == "yes":
+
+        if step == "get_bag" and sub_hand_cnt==0:
             # yolov8 detect
-            
+
             # pose detect
             A = []
             B = []
@@ -523,37 +529,46 @@ if __name__ == "__main__":
                 YN = -1
                 a_num, b_num = 9, 7
                 for issack in range(len(poses)):
-                    yu=0
+                    yu = 0
                     if poses[issack][9][2] > 0 and poses[issack][7][2] > 0:
                         YN = 0
-                        
-                        a_num, b_num = 9,7
+
+                        a_num, b_num = 9, 7
                         A = list(map(int, poses[issack][a_num][:2]))
                         if (640 >= A[0] >= 0 and 320 >= A[1] >= 0):
-                            ax, ay, az = get_real_xyz(up_depth, A[0], A[1],2)
-                            if az<=2500 and az!=0:
+                            ax, ay, az = get_real_xyz(up_depth, A[0], A[1], 2)
+                            if az <= 2500 and az != 0:
                                 yu += 1
                         B = list(map(int, poses[issack][b_num][:2]))
                         if (640 >= B[0] >= 0 and 320 >= B[1] >= 0):
-                            bx, by, bz = get_real_xyz(up_depth, B[0], B[1],2)
-                            if bz<=2500 and bz!=0:
+                            bx, by, bz = get_real_xyz(up_depth, B[0], B[1], 2)
+                            if bz <= 2500 and bz != 0:
                                 yu += 1
-                    if yu>=2:
+                    if yu >= 2:
                         break
             print(A, B)
-            if len(A) != 0 and yu >= 2 and len(B) !=0:
+            if len(A) != 0 and yu >= 2 and len(B) != 0:
                 cv2.circle(up_image, (A[0], A[1]), 3, (0, 255, 0), -1)
                 cv2.circle(up_image, (B[0], B[1]), 3, (0, 255, 0), -1)
-                _sub_up_cam_image.unregister()
-                _sub_up_cam_depth.unregister()
-                up_cam_open="no"
-                down_cam_open="yes"
-                time.sleep(2)
-                
-                
+                target_x=bx
+                if sub_hand_cnt == 0:
+                    #_sub_down_cam_image.unregister()
+                    #_sub_down_cam_depth.unregister()
+                    _sub_up_cam_image.unregister()
+                    _sub_up_cam_depth.unregister()
+                    print("close up open down")
+                    time.sleep(2)
+
+                    _sub_down_cam_depth = rospy.Subscriber("/cam1/depth/image_raw", Image, callback_depth2)
+                    _sub_down_cam_image = rospy.Subscriber("/cam1/color/image_raw", Image, callback_image2)
+                    #_sub_up_cam_image = rospy.Subscriber("/cam2/color/image_raw", Image, callback_image1)
+                    #_sub_up_cam_depth = rospy.Subscriber("/cam2/depth/image_raw", Image, callback_image1)
+
+                    time.sleep(2)
+                    sub_hand_cnt += 1
 
         # if step=="none": continue
-        if step == "get_bag" and down_cam_open="yes":
+        if step == "get_bag" and sub_hand_cnt>=1:
             detections = dnn_yolo1.forward(down_image)[0]["det"]
             for i, detection in enumerate(detections):
                 # print(detection)
@@ -566,66 +581,65 @@ if __name__ == "__main__":
                     cv2.rectangle(down_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
                     cv2.circle(down_image, (cx, cy), 5, (0, 255, 0), -1)
                     print("bag score:", score)
-            if len(A) != 0 and len(B) != 0:
 
-                bx = A[0]
-                if len(detection_list) < 1:
-                    print("no bag")
+            bx = target_x
+            if len(detection_list) < 1:
+                print("no bag")
 
-                if len(detection_list) <= 2 and len(detection_list) > 0:
-                    sort_detection = sorted(detection_list, key=(lambda x: x[0]))
-                    print(detection_list)
-                    if len(detection_list) == 1:
+            if len(detection_list) <= 2 and len(detection_list) > 0:
+                sort_detection = sorted(detection_list, key=(lambda x: x[0]))
+                print(detection_list)
+                if len(detection_list) == 1:
+                    need_position = sort_detection[0]
+                else:
+
+                    if posecnt == 0:
+                        if bx < 320:
+                            lr = "left"
+                        else:
+                            lr = "right"
+                        posecnt += 1
+                    if lr == "left":
                         need_position = sort_detection[0]
                     else:
+                        need_position = sort_detection[1]
 
-                        if posecnt == 0:
-                            if bx < 320:
-                                lr = "left"
-                            else:
-                                lr = "right"
-                            posecnt += 1
-                        if lr == "left":
-                            need_position = sort_detection[0]
-                        else:
-                            need_position = sort_detection[1]
+                print(need_position)
+                print("ho")
+                x1, y1, x2, y2, cx2, cy2 = map(int, need_position)
+                cv2.rectangle(down_image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                cv2.circle(down_image, (cx2, cy2), 5, (0, 0, 255), -1)
 
-                    print(need_position)
-                    print("ho")
+                # capture evidence
+                now = datetime.datetime.now()
+                filename = now.strftime("%Y-%m-%d_%H-%M-%S.jpg")
+                output_dir = "/home/pcms/catkin_ws/src/beginner_tutorials/src/m1_evidence/"
+                cv2.imwrite(output_dir + filename, down_image)
+                if move_turn == "turn":
+
+                    h, w, c = down_image.shape
                     x1, y1, x2, y2, cx2, cy2 = map(int, need_position)
-                    cv2.rectangle(down_image, (x1, y1), (x2, y2), (0, 0, 255), 2)
-                    cv2.circle(down_image, (cx2, cy2), 5, (0, 0, 255), -1)
-
-                    # capture evidence
-                    now = datetime.datetime.now()
-                    filename = now.strftime("%Y-%m-%d_%H-%M-%S.jpg")
-                    output_dir = "/home/pcms/catkin_ws/src/beginner_tutorials/src/m1_evidence/"
-                    cv2.imwrite(output_dir + filename, down_image)
-                    if move_turn == "turn":
-
-                        h, w, c = down_image.shape
-                        x1, y1, x2, y2, cx2, cy2 = map(int, need_position)
-                        e = w // 2 - cx2
-                        v = 0.001 * e
-                        if v > 0:
-                            v = min(v, 0.3)
-                        if v < 0:
-                            v = max(v, -0.3)
-                        move(0, v)
-                        print(e)
-                        if abs(e) <= 3:
-                            say("walk")
-                            action = "front"
-                            move_turn = "none"
-                            step = "none"
-                            print("ys")
-                            #break
-                            #break
+                    e = w // 2 - cx2
+                    v = 0.001 * e
+                    if v > 0:
+                        v = min(v, 0.3)
+                    if v < 0:
+                        v = max(v, -0.3)
+                    move(0, v)
+                    print(e)
+                    if abs(e) <= 3:
+                        say("walk")
+                        action = "front"
+                        move_turn = "none"
+                        step = "none"
+                        print("ys")
+                        # break
+                        # break
 
         if step == "check_voice":
-            s=s.lower()
+            s = s.lower()
             if "thank" in s or "you" in s:
-                action = "none" 
+                action = "none"
                 say("I will go back now, bye bye")
                 joint1, joint2, joint3, joint4 = 0.000, 0.0, 0, 1.5
                 set_joints(joint1, joint2, joint3, joint4, 1)
@@ -683,31 +697,38 @@ if __name__ == "__main__":
             set_joints(joint1, joint2, joint3, joint4, 1)
             time.sleep(t)
 
-            joint1, joint2, joint3, joint4 = 0.087,0.119, 0.365, -1.4
+            joint1, joint2, joint3, joint4 = 0.087, 0.119, 0.365, -1.4
             set_joints(joint1, joint2, joint3, joint4, t)
             time.sleep(t)
-            joint1, joint2, joint3, joint4 = 0.087,0.0, 0.365, -1.4
+            joint1, joint2, joint3, joint4 = 0.087, 0.0, 0.365, -1.4
             set_joints(joint1, joint2, joint3, joint4, t)
             time.sleep(t)
-            #joint1, joint2, joint3, joint4 = 0, 0, 0, -1.0
-            #set_joints(joint1, joint2, joint3, joint4, t)
-            #time.sleep(t)
+            # joint1, joint2, joint3, joint4 = 0, 0, 0, -1.0
+            # set_joints(joint1, joint2, joint3, joint4, t)
+            # time.sleep(t)
             time.sleep(3)
             say("I will follow you now")
             for i in range(50000): move(-0.2, 0)
+
             action = "follow"
 
         if action == "follow":
             print('follow')
-            if follow_cnt==0:
+            if sub_follow_cnt == 0:
                 _sub_down_cam_image.unregister()
                 _sub_down_cam_depth.unregister()
-                _topic_image1 = "/cam2/color/image_raw"
-                _sub_up_cam_image = rospy.Subscriber(_topic_image1, Image, callback_image1)
-                _topic_depth1 = "/cam2/depth/image_raw"
-                _sub_up_cam_image = rospy.Subscriber(_topic_image1, Image, callback_image1)
-                follow_cnt+=1
-            
+                #_sub_up_cam_image.unregister()
+                #_sub_up_cam_depth.unregister()
+
+                time.sleep(2)
+                print("close down open up")
+                #_sub_down_cam_depth = rospy.Subscriber("/cam1/depth/image_raw", Image, callback_depth2)
+                #_sub_down_cam_image = rospy.Subscriber("/cam1/color/image_raw", Image, callback_image2)
+                _sub_up_cam_image = rospy.Subscriber("/cam2/color/image_raw", Image, callback_image1)
+                _sub_up_cam_depth = rospy.Subscriber("/cam2/depth/image_raw", Image, callback_image1)
+
+                time.sleep(2)
+                sub_follow_cnt += 1
             msg = Twist()
             '''
             detections = dnn_yolo2.forward(up_image)[0]["det"]
@@ -730,26 +751,27 @@ if __name__ == "__main__":
                     print("bag score:", score)
                     nx=d
                     cx_n, cy_n = cx,cy'''
-                    
+
             poses = net_pose.forward(up_image)
             min_d = 9999
+            t_idx = -1
             for i, pose in enumerate(poses):
                 if pose[5][2] == 0 or pose[6][2] == 0:
                     continue
                 p5 = list(map(int, pose[5][:2]))
                 p6 = list(map(int, pose[6][:2]))
-                if d>=1800 or d == 0: continue
+                if d >= 1800 or d == 0: continue
                 cx = (p5[0] + p6[0]) // 2
                 cy = (p5[1] + p6[1]) // 2
                 cv2.circle(up_image, p5, 5, (0, 0, 255), -1)
                 cv2.circle(up_image, p6, 5, (0, 0, 255), -1)
                 cv2.circle(up_image, (cx, cy), 5, (0, 255, 0), -1)
                 _, _, d = get_real_xyz(up_depth, cx, cy, 2)
-                
-                if (d != 0 and d < min_d) and d<=1800:
+
+                if (d != 0 and d < min_d) and d <= 1800:
                     t_idx = i
                     min_d = d
-            
+
             x, z = 0, 0
             if min_d != 9999:
                 p5 = list(map(int, poses[t_idx][5][:2]))
@@ -758,15 +780,14 @@ if __name__ == "__main__":
                 cy = (p5[1] + p6[1]) // 2
                 _, _, d = get_real_xyz(up_depth, cx, cy, 2)
                 cv2.circle(up_image, (cx, cy), 5, (0, 255, 255), -1)
-                
-                print("people_d",d)
-                if d>=1800 or d == 0: continue
-                
-                
+
+                print("people_d", d)
+                if d >= 1800 or d == 0: continue
+
                 x, z, up_image, yn = _fw.calc_cmd_vel(up_image, up_depth, cx, cy)
                 print("turn_x_z:", x, z)
             move(x, z)
-                    
+
             '''
             A = []
             B = []
@@ -782,7 +803,7 @@ if __name__ == "__main__":
                     yu=0
                     if poses[issack][5][2] > 0 and poses[issack][6][2] > 0:
                         YN = 0
-                        
+
                         a_num, b_num = 5,6
                         A = list(map(int, poses[issack][a_num][:2]))
                         B = list(map(int, poses[issack][b_num][:2]))
@@ -804,12 +825,12 @@ if __name__ == "__main__":
             if len(MB)!=0:
                 cv2.circle(up_image, (MB[0], MB[1]), 3, (0, 255, 0), -1)
             if len(MA)!=0 and len(MB)!=0:
-                
+
                 cv2.line(up_image, (MA[0], MA[1]), (MB[0], MB[1]), (0, 0, 255), 2)
                 if yu>=2:
                     x, z, up_image, yn = _fw.calc_cmd_vel(up_image, up_depth, aacx,aacy)
                     print("turn_x_z:", x, z)
-                    
+
 
                     move(x, z)
                     x, z = 0, 0
@@ -821,12 +842,12 @@ if __name__ == "__main__":
                 slocnt += 1
             #step = "check_voice"
             '''
-            
+
         if action == "back3":
             break
             clear_costmaps
-            #chassis.move_to(-0.703,-4.65,0)
-            chassis.move_to(5.51,6.74,0)
+            # chassis.move_to(-0.703,-4.65,0)
+            chassis.move_to(5.51, 6.74, 0)
             # checking
             while not rospy.is_shutdown():
                 # 4. Get the chassis status.
@@ -836,8 +857,8 @@ if __name__ == "__main__":
                     break
             time.sleep(1)
             clear_costmaps
-            #chassis.move_to(-2.36,3.43,0)
-            chassis.move_to(9.21,5.51,0)
+            # chassis.move_to(-2.36,3.43,0)
+            chassis.move_to(9.21, 5.51, 0)
             # checking
             while not rospy.is_shutdown():
                 # 4. Get the chassis status.
@@ -847,8 +868,8 @@ if __name__ == "__main__":
                     break
             time.sleep(1)
             clear_costmaps
-            #chassis.move_to(-0.531,-2.99,0.201)
-            chassis.move_to(7.95,8.08,0)
+            # chassis.move_to(-0.531,-2.99,0.201)
+            chassis.move_to(7.95, 8.08, 0)
             # checking
             while not rospy.is_shutdown():
                 # 4. Get the chassis status.
